6a9aa80eb1da46ebc842ec11f0fedf4cdf25e4ed
tidianeaw <tidiane_aw@yahoo.fr>
2022-11-21 16:48:36 +0000

    First commit

--- test_networkx.py
+++ test_networkx.py
@@ -5,18 +5,17 @@ Created on Mon Nov 21 14:17:26 2022
 @author: tidiane
 """
 
-"""
-Import librairie networkx : alias = netx
-"""
+
+#Import librairie networkx : alias = netx
+
 import networkx as netx
 G = netx.Graph()
-"""
-ajout de noeud
-"""
+
+#ajout de noeud
 G.add_node(1)
-"""
-ajout de noeuds avec code couleur
-"""
+
+#ajout de noeuds avec code couleur
+
 G.add_nodes_from([
     (2, {"color": "red"}),
     (3, {"color": "green"}),
