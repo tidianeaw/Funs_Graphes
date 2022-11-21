e2ab3a934f5778904e0954dd3aa14be5b75f887c
tidianeaw <tidiane_aw@yahoo.fr>
2022-11-21 18:11:23 +0000

    2nd commit

--- test_networkx.py
+++ test_networkx.py
@@ -9,14 +9,10 @@ Created on Mon Nov 21 14:17:26 2022
 #Import librairie networkx : alias = netx
 
 import networkx as netx
-G = netx.Graph()
+import matplotlib.pyplot as plt
 
-#ajout de noeud
-G.add_node(1)
-
-#ajout de noeuds avec code couleur
-
-G.add_nodes_from([
-    (2, {"color": "red"}),
-    (3, {"color": "green"}),
-])
+G = netx.petersen_graph()
+subax1 = plt.subplot(121)
+netx.draw(G, with_labels=True, font_weight='bold')
+subax2 = plt.subplot(122)
+netx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
